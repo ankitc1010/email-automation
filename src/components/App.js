932fbddc82fe43da1cb896fd381c26dd1f3f0168
Ankit Chauhan <ankit.chauhan@unifize.com>
2019-02-18 13:48:59 +0530

    Zoho integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import '../assets/css/App.css'
-import React, { Component } from 'react'
+import "../assets/css/App.css";
+import React, { Component } from "react";
+import Zoho from "zoho";
+
+var invoice = new Zoho.Invoice({
+  authtoken: "4df39abf5d3b434814bcadb0e79ebcf4"
+});
+
+console.log(invoice);
+console.log("here");
+
+invoice.getRecords("contacts", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello, Electron!</h1>
-        <p>cool</p>
 
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <p>
+          I hope you enjoy using basic-electron-react-boilerplate to start your
+          dev off right!
+        </p>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
